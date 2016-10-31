9b31adf343a8f35587fa483ce274921a32cec629
libaoyu <muzibaoyu91@gmail.com>
2016-10-31 10:46:17 +0800

    Initial Commit

---
+++ millcolorGradQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  millcolorGradQRCode
+//
+//  Created by Baoyu on 2016/10/31.
+//  Copyright © 2016年 Baoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
